fec97e63ca078346c51f3fbac98ac87402da1b88
termie <github@anarkystic.com>
2013-07-17 15:59:41 -0700

    less colors, more options

--- fabric_gunicorn.py
+++ fabric_gunicorn.py
@@ -1,121 +1,155 @@
-# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
 # fabric-gunicorn
 # Copyright: (c) 2012 Christoph Heer <Christoph.Heer@googlemail.com>
 # License: BSD, see LICENSE for more details.
 
-from time import sleep
+import time
 
-from fabric import colors
-from fabric.api import task, env, run, cd
-from fabric.utils import abort, puts
+from fabric.api import cd
+from fabric.api import env
+from fabric.api import run
+from fabric.api import sudo
+from fabric.api import task
 from fabric.context_managers import hide
+from fabric.utils import abort
+from fabric.utils import puts
 
 
-def set_env_defaults():
-    env.setdefault('remote_workdir', '~')
-    env.setdefault('gunicorn_pidpath', env.remote_workdir + '/gunicorn.pid')
+def _set_env_defaults():
+    env.setdefault('gunicorn_remote_workdir', '~')
+    env.setdefault('gunicorn_pidpath',
+                   env.gunicorn_remote_workdir + '/gunicorn.pid')
     env.setdefault('gunicorn_bind', '127.0.0.1:8000')
+    env.setdefault('gunicorn_works', 1)
+    env.setdefault('gunicorn_user', None)
+    env.setdefault('gunicorn_group', None)
+    env.setdefault('gunicorn_worker_class', 'egg:gunicorn#sync')
+    env.setdefault('gunicorn_mode', None)
+    env.setdefault('gunicorn_wsgi_app', None)
+    env.setdefault('gunicorn_pythonpath', None)
+    env.setdefault('gunicorn_django_settings', None)
+    env.setdefault('gunicorn_paster_config', None)
 
-set_env_defaults()
+
+_set_env_defaults()
 
 
-def gunicorn_running():
-    return run('ls ' + env.gunicorn_pidpath, quiet=True).succeeded
+def running():
+    return sudo('ls ' + env.gunicorn_pidpath, quiet=True).succeeded
 
 
-def gunicorn_running_workers():
+def running_workers():
     count = None
     with hide('running', 'stdout', 'stderr'):
-        count = run('ps -e -o ppid | grep `cat %s` | wc -l' %
-                    env.gunicorn_pidpath)
+        count = sudo('ps -e -o ppid | grep `cat %s` | wc -l' %
+                     env.gunicorn_pidpath)
     return count
 
 
 @task
 def status():
-    """Show the current status of your Gunicorn process"""
-
-    set_env_defaults()
-
-    if gunicorn_running():
-        puts(colors.green("Gunicorn is running."))
-        puts(colors.yellow('Active workers: %s' % gunicorn_running_workers()))
+    """Show the current status of your gunicorn process"""
+    if running():
+        puts('gunicorn is running.')
+        puts('active workers: %s' % running_workers())
     else:
-        puts(colors.blue("Gunicorn isn't running."))
+        puts('gunicorn is not running.')
 
 
 @task
 def start():
-    """Start the Gunicorn process"""
-
-    set_env_defaults()
-
-    if gunicorn_running():
-        puts(colors.red("Gunicorn is already running!"))
+    """Start the gunicorn process"""
+    if running():
+        puts('gunicorn is already running.')
         return
 
-    if 'gunicorn_wsgi_app' not in env:
-        abort(colors.red('env.gunicorn_wsgi_app not defined'))
+    mode = env.get('gunicorn_mode')
+    if not mode:
+        if env.get('gunicorn_paster_config'):
+            mode = 'paster'
+        elif env.get('gunicorn_django_settings'):
+            mode = 'django'
+        else:
+            mode = 'wsgi'
 
-    with cd(env.remote_workdir):
-        prefix = []
-        if 'virtualenv_dir' in env:
-            prefix.append('source %s/bin/activate' % env.virtualenv_dir)
-        if 'django_settings_module' in env:
-            prefix.append('export DJANGO_SETTINGS_MODULE=%s' %
-                          env.django_settings_module)
+    if mode == 'wsgi' and not env.gunicorn_wsgi_app:
+        abort('env.gunicorn_wsgi_app is not defined.')
 
-        prefix_string = ' && '.join(prefix)
-        if len(prefix_string) > 0:
-            prefix_string += ' && '
+    if mode == 'django' and not env.gunicorn_django_settings:
+        abort('env.gunicorn_django_settings is not defined.')
+
+    if mode == 'paster' and not env.gunicorn_paster_config:
+        abort('env.gunicorn_paster_config is not defined.')
+
+
+    with cd(env.gunicorn_remote_workdir):
+        #prefix = []
+        #if 'virtualenv_dir' in env:
+        #    prefix.append('source %s/bin/activate' % env.virtualenv_dir)
+        #
+        #prefix_string = ' && '.join(prefix)
+        #if len(prefix_string) > 0:
+        #    prefix_string += ' && '
 
         options = [
             '--daemon',
+            '--error-logfile %s/error.log' % env.gunicorn_remote_workdir,
             '--pid %s' % env.gunicorn_pidpath,
             '--bind %s' % env.gunicorn_bind,
         ]
-        if 'gunicorn_workers' in env:
-            options.append('--workers %s' % env.gunicorn_workers)
-        if 'gunicorn_worker_class' in env:
-            options.append('--worker-class %s' % env.gunicorn_worker_class)
+
+        for option in ('pythonpath', 'user', 'group', 'workers',
+                       'worker-class'):
+            attr = getattr(env, 'gunicorn_%s' % option.replace('-', '_'))
+            if attr:
+                options.append('--%s %s' % (option, attr))
+
         options_string = ' '.join(options)
 
-        if 'paster_config_file' in env:
-            run('%s gunicorn_paster %s %s' % (prefix_string, options_string,
-                                   env.paster_config_file))
+        # we need to sudo if we're going to run as a specific user
+        if env.gunicorn_user:
+            runner = sudo
         else:
-            run('%s gunicorn %s %s' % (prefix_string, options_string,
-                                   env.gunicorn_wsgi_app))
+            runner = run
 
-        if gunicorn_running():
-            puts(colors.green("Gunicorn started."))
+        # actually run the stuff
+        if mode == 'paster':
+            runner('gunicorn_paster %s %s'
+                   % (options_string, env.gunicorn_paster_config))
+        elif mode == 'django':
+            runner('gunicorn_django %s %s'
+                   % (options_string, env.gunicorn_django_settings), pty=False)
+        elif mode == 'wsgi':
+            runner('gunicorn %s %s'
+                   % (options_string, env.gunicorn_wsgi_app))
+
+        if running():
+            puts('gunicorn started.')
         else:
-            abort(colors.red("Gunicorn wasn't started!"))
+            abort('gunicorn was not started.')
 
 
 @task
 def stop():
     """Stop the Gunicorn process"""
-
-    set_env_defaults()
-
-    if not gunicorn_running():
-        puts(colors.red("Gunicorn isn't running!"))
+    if not running():
+        puts('gunicorn is not running!')
         return
 
-    run('kill `cat %s`' % (env.gunicorn_pidpath))
+    sudo('kill `cat %s`' % (env.gunicorn_pidpath))
 
     for i in range(0, 5):
         puts('.', end='', show_prefix=i == 0)
 
-        if gunicorn_running():
-            sleep(1)
+        if running():
+            time.sleep(1)
         else:
             puts('', show_prefix=False)
-            puts(colors.green("Gunicorn was stopped."))
+            puts('gunicorn was stopped.')
             break
     else:
-        puts(colors.red("Gunicorn wasn't stopped!"))
+        puts('gunicorn was not stopped')
         return
 
 
@@ -129,36 +163,32 @@ def restart():
 @task
 def reload():
     """Gracefully reload the Gunicorn process and the wsgi application"""
-
-    set_env_defaults()
-    if not gunicorn_running():
-        puts(colors.red("Gunicorn isn't running!"))
+    if not running():
+        puts('gunicorn is not running.')
         return
-    puts(colors.yellow('Gracefully reloading Gunicorn...'))
+    puts('Reloading gunicorn...')
     run('kill -HUP `cat %s`' % (env.gunicorn_pidpath))
 
 
 @task
 def add_worker():
     """Increase the number of your Gunicorn workers"""
-    set_env_defaults()
-    if not gunicorn_running():
-        puts(colors.red("Gunicorn isn't running!"))
+    if not running():
+        puts('gunicorn is not running!')
         return
 
-    puts(colors.green('Increasing number of workers...'))
+    puts('Increasing number of workers...')
     run('kill -TTIN `cat %s`' % (env.gunicorn_pidpath))
-    puts(colors.yellow('Active workers: %s' % gunicorn_running_workers()))
+    puts('Active workers: %s' % running_workers())
 
 
 @task
 def remove_worker():
     """Decrease the number of your Gunicorn workers"""
-    set_env_defaults()
-    if not gunicorn_running():
-        puts(colors.red("Gunicorn isn't running!"))
+    if not running():
+        puts('gunicorn is not running!')
         return
 
-    puts(colors.green('Decreasing number of workers...'))
+    puts('Decreasing number of workers...')
     run('kill -TTOU `cat %s`' % (env.gunicorn_pidpath))
-    puts(colors.yellow('Active workers: %s' % gunicorn_running_workers()))
+    puts('Active workers: %s' % running_workers())
